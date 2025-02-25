d8deef88b5930580984cd03f2fe638e1779c003b
Vitor <vitordevestudos@gmail.com>
2025-02-24 22:39:51 -0300

    aula08

---
+++ PHP-PinguimAcademy/aula08/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minha aula de php</title>
+</head>
+<body>
+    <h1>
+
+        <?php
+            $saudacao = "Olá";
+            $localidade = "Mundo!";
+            $numero = 1;
+            $nome = "Vitor";
+            echo $saudacao . " " . $localidade . " " . $numero;
+            echo "Olá " . $nome . "!";
+        ?>
+    </h1>
+</body>
+</html>
